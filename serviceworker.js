132053421e7cf8a1ca026c78080d001bb33c07f7
soporte-web <soporte-web@users.noreply.github.com>
2021-12-03 11:48:00 -0300

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "APP Unitec";
+const urlsToCache = ["/", "style.css", "js/script.js", "imgs/logo.png", "imgs/mask.png"];
+
+self.addEventListener("install", event => {
+    // Pre-cache archivos para offline y performance; los pictogramas los dejamos para la primera carga
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache).then( async () => {});
+        })
+    );
+});
+// Cache First Policy
+self.addEventListener("fetch", event => {
+  event.respondWith(
+      caches.match(event.request)  
+          .then(function(response) {
+              if (response) {  // CACHE HIT
+                  return response;
+              } else {    // CACHE MISS
+                  return fetch(event.request);
+              }
+          })
+  );
+});
